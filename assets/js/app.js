624bb578b144e95dd95a68456bb2ea83964bd85f
C Blackwell <bmathematics@gmail.com>
2021-03-23 18:38:44 -0400

    forex javascript

---
+++ assets/js/app.js
@@ -0,0 +1,39 @@
+var destinationEl = document.getElementById ("userDestination");
+var toCountryEl = document.querySelector ("#toCountry");
+var startAmountEl = document.getElementById ("startAmount");
+var endAmountEl = document.getElementById ("endAmount");
+var buttonEl = document.getElementById ("btn");
+var code;
+
+
+buttonEl.addEventListener("click", function(){
+    // function convertCountry(){
+        toCountryEl.value = destinationEl.value;
+        console.log(typeof parseInt(startAmountEl.value));
+    //put search country into input Destination
+    if(typeof parseInt(startAmountEl.value) !== "number")return;
+    
+    fetch (`https://restcountries.eu/rest/v2/name/${destinationEl.value}`)
+        .then (function (response){
+            return response.json()
+        })
+        .then(function(data){
+            code = (data[0].currencies[0].code);
+            symbol = (data[0].currencies[0].symbol)
+            console.log(data)
+
+
+            // document.querySelector("#toCountry").textContent = code;
+        })
+    .then (function(){
+            fetch(`https://v6.exchangerate-api.com/v6/96d07d67e0f9e9f46600886f/pair/USD/${code}/${startAmountEl.value}`)
+        .then(function(response){
+            return response.json()
+        })
+        .then(function(data){
+            console.log(data);
+            endAmountEl.value =`${symbol}${data.conversion_result}`;
+        });
+    })
+
+});
